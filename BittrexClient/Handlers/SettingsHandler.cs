191d2b3ecd8aa11db3a9a3cb81752a0b9da7c3e7
Grzegorz Pietrzak <grzegorz.pietrzak321@gmail.com>
2018-07-04 23:45:55 +0200

    settings draft added

--- BittrexClient/Handlers/SettingsHandler.cs
+++ BittrexClient/Handlers/SettingsHandler.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BittrexClient.Models;
 
 namespace BittrexClient.Handlers
 {
     class SettingsHandler
     {
+        public void Save(string market, List<string> currenciesList, string readkey, string tradeKey, string withdrawkey)
+        {
+
+        }
+
+        public void Load()
+        {
+
+        }
     }
 }
---
+++ BittrexClient/Models/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BittrexClient.Models
+{
+    class Settings
+    {
+        public string Market { get; set; }
+        public List<String> CurrenciesList { get; set; }
+        public string ReadKey { get; set; }
+        public string TradeKey { get; set; }
+        public string WithdrawKey { get; set; }
+    }
+}
